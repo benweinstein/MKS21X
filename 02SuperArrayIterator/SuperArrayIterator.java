cabbb7fa20890870d4c3de70ff89fb0a00d64268
Ben Weinstein <bweinstein@stuy.edu>
2016-12-02 05:49:02 -0500

    Added Mr. K's Driver.java, basics of Iterator (v2)

--- 02SuperArrayIterator/SuperArrayIterator.java
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -1,7 +1,35 @@
-import java.util.Iterator;
+import java.util.*;
 
 public class SuperArrayIterator implements Iterator<String>{
-    
+    private int start;
+    private int end;
+    private SuperArray sAry;
+    //^^ need something for the SuperArray ^^
+
+    /**Constructor!!!*/
+    public SuperArrayIterator(int start, int end, SuperArray sAry){
+	this.start = start;
+	this.end = end;
+	this.sAry = sAry;
+    }
+
+    public String next(){
+	if(hasNext()){
+	    start++;
+	    return sAry.get(start - 1);
+	}
+	else{
+	    throw new NoSuchElementException();
+	}
+    }
+
+    public boolean hasNext(){
+	return start < end && start < sAry.size();
+    }
+
+    public void remove(){
+	throw new UnsupportedOperationException();
+    }
 
 
 
