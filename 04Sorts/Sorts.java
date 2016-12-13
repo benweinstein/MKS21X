d052503ce9987a1a8f5c89c1c5352c6cece5f4a5
Ben Weinstein <bweinstein@stuy.edu>
2016-12-13 14:30:28 -0500

    Copied framework, edited name() (v1)

---
+++ 04Sorts/Sorts.java
@@ -0,0 +1,18 @@
+public class Sorts{
+  /**EDIT THIS METHOD TO MATCH YOUR NAME 
+  */
+  public static String name(){
+    return "01.Weinstein.Ben"; 
+  }
+
+  /**Selection sort of an int array. 
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static void selectionSort(int[] data){ 
+      
+
+
+  }
+
+}
