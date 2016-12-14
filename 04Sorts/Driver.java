fae78fae190ea77e6ba539143df4f6986d341ed8
Ben Weinstein <bweinstein@stuy.edu>
2016-12-14 14:46:25 -0500

    Doesn't work... (v1)

--- 04Sorts/Driver.java
+++ 04Sorts/Driver.java
@@ -5,5 +5,11 @@ public class Driver{
 	for(int i = 0; i < data.length; i++){
 	    System.out.println(data[i]);
 	} //works!!!
+
+	int[] data1 = {3,2,5,6,2,7,9};
+	Sorts.insertionSort(data1);
+	for(int i: data1){
+	    System.out.println(data1[i]);
+	}
     }
 }
--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,25 +1,48 @@
 public class Sorts{
-  /**EDIT THIS METHOD TO MATCH YOUR NAME 
-  */
-  public static String name(){
-    return "01.Weinstein.Ben"; 
-  }
+    /**EDIT THIS METHOD TO MATCH YOUR NAME 
+     */
+    public static String name(){
+	return "01.Weinstein.Ben"; 
+    }
+    
+    /**Selection sort of an int array. 
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
+    public static void selectionSort(int[] data){ 
+	for(int i = 0; i < data.length; i++){//length - 1? for last element...
+	    int currentMinIndex = i;
+	    for(int j = i + 1; j < data.length; j++){
+		if(data[i] > data[j]){
+		    currentMinIndex = j;
+		}
+	    }
+	    int helperInt = data[i];
+	    data[i] = data[currentMinIndex];
+	    data[currentMinIndex] = helperInt;
+	}
+    }
 
-  /**Selection sort of an int array. 
-  *Upon completion, the elements of the array will be in increasing order.
-  *@param data  the elements to be sorted.
-  */
-  public static void selectionSort(int[] data){ 
-      for(int i = 0; i < data.length; i++){//length - 1? for last element...
-	  int currentMinIndex = i;
-	  for(int j = i + 1; j < data.length; j++){
-	      if(data[i] > data[j]){
-		  currentMinIndex = j;
-	      }
-	  }
-	  int helperInt = data[i];
-	  data[i] = data[currentMinIndex];
-	  data[currentMinIndex] = helperInt;
-      }
-  }
+    /**Insertion sort of an int array.
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
+    public static void insertionSort(int[] data){
+	for(int i = 1; i < data.length; i++){
+	    if(data[i] < data[i - 1]){
+		int toBeSwitched = data[i]; 
+		int indexToStop = i - 1;
+		for(int j = i - 1; j >= 0; j--){
+		    if(toBeSwitched < data[j]){
+			indexToStop = j; 
+		    }		    
+		}
+		data[indexToStop] = toBeSwitched;
+		for(int index = data.length - 1; index > indexToStop; index--){
+		    data[index] = data[index - 1];
+		}
+		i--;
+	    }
+	}
+    }
 }
