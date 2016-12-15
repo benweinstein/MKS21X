1b416e599bf8d725efd50b94c2702f09ae00385f
Ben Weinstein <bweinstein@stuy.edu>
2016-12-14 23:50:59 -0500

    Fixed name() to include period (v6)

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -2,7 +2,7 @@ public class Sorts{
     /**EDIT THIS METHOD TO MATCH YOUR NAME 
      */
     public static String name(){
-	return "01.Weinstein.Ben"; 
+	return "09.Weinstein.Ben"; 
     }
     
     /**Selection sort of an int array. 
